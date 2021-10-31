76d60ffe9a0d8117227686a485c455fbe15c2fd4
Damien <thecsmine@gmail.com>
2021-10-31 11:47:29 +0100

    Formatting and norm()

--- src/Vector.java
+++ src/Vector.java
@@ -11,9 +11,8 @@ public class Vector {
     }
 
     /**
-     * 
-     * @param v1
-     * @param v2
+     * @param v1 : Vector
+     * @param v2 : Vector
      * @return Sum of the 2 vectors in a new Vector
      */
     public static Vector add(Vector v1, Vector v2) {
@@ -21,9 +20,8 @@ public class Vector {
     }
 
     /**
-     *
-     * @param v1
-     * @param v2
+     * @param v1 : Vector
+     * @param v2 : Vector
      * @return Substraction of the 2 vectors in a new Vector
      */
     public static Vector sub(Vector v1, Vector v2) {
@@ -33,17 +31,17 @@ public class Vector {
     /**
      * Sum the vector to itself and modify directly its value
      *
-     * @param v
+     * @param v : Vector
      */
     public void add(Vector v) {
         this.setX(this.getX() + v.getX());
         this.setY(this.getY() + v.getY());
     }
 
-	/**
+    /**
      * Substract the vector to itself and modify directly its value
      *
-     * @param v
+     * @param v : Vector
      */
     public void sub(Vector v) {
         this.setX(this.getX() - v.getX());
@@ -51,26 +49,31 @@ public class Vector {
     }
 
 
-
     /**
      * Modify and multiply all the values of the vector by m
      *
-     * @param m
+     * @param m : float
      */
     public void mult(float m) {
         this.x *= m;
         this.y *= m;
     }
-    
-    
+
+
     /**
-     * 
-     * @param v1
-     * @param v2
+     * @param v1 : Vector
+     * @param v2 : Vector
      * @return Distance between points represented by *v1* and *v2*
      */
     public static float distance(Vector v1, Vector v2) {
         return (float) Math.sqrt(Math.pow(v1.getX() - v2.getX(), 2) + Math.pow(v1.getY() - v2.getY(), 2));
+    }
+
+    /**
+     * @return Norm of the calling Vector
+     */
+    public float norm() {
+        return (float) Math.sqrt(Math.pow(this.getX(), 2) + Math.pow(this.getY(), 2));
     }
 
     public float getX() {
@@ -88,10 +91,10 @@ public class Vector {
     public void setY(float y) {
         this.y = y;
     }
-    
+
 
     @Override
-	public String toString() {
-		return "{x=" + x + ", y=" + y + "}";
-	}
+    public String toString() {
+        return "{x=" + x + ", y=" + y + "}";
+    }
 }
