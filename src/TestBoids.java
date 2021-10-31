086cb0c89ca0b3383f17a1e16cc2aaddb4a3a613
Damien <thecsmine@gmail.com>
2021-10-31 14:21:05 +0100

    Change bg color and init boids

--- src/TestBoids.java
+++ src/TestBoids.java
@@ -4,12 +4,11 @@ import java.awt.*;
 
 public class TestBoids {
     public static void main(String[] args) {
-        GUISimulator gui = new GUISimulator(500, 500, Color.BLACK);
+        GUISimulator gui = new GUISimulator(500, 500, Color.decode("#b1eefe"));
         Boids[] boids = {
-                new Boids(1, 1, 5),
-                new Boids(5, 2, 10),
-                new Boids(4, 1, 2),
-                new Boids(7, 3, 6),
+                new Boids(100, 100, 50),
+                new Boids(400, 200, 50),
+                new Boids(300, 300, 50),
         };
         gui.setSimulable(new BoidsSimulator(gui, boids));
     }
