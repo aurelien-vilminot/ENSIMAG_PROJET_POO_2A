bb686a2d43210366a6a23e5d6d1a003855b7048f
Damien <thecsmine@gmail.com>
2021-11-05 11:28:41 +0100

    Move rules to Boids ; add attributes xMax and yMax
    + xMax
    + yMax
    + rule[...](ArrayList<Boids>)
    + applyRules(ArrayList<Boids>)
    + updateVelocity()
    + updatePosition()

--- src/Boids.java
+++ src/Boids.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 /**
  * Creation of boids.
  */
@@ -6,14 +8,18 @@ public class Boids {
     private Vector velocity;
     private Vector acceleration;
     private float detectionRadius;
+    private float xMax;
+    private float yMax;
 
     /**
      * Constructor of boids
      * @param x : initial position of boids on the axis X.
      * @param y : initial position of boids on the axis Y.
-     * @param detectionRadius : how the boid detects tis environnment.
+     * @param detectionRadius : how the boid detects its environnment.
+     * @param xMax : maximum position of boids on the axis X.
+     * @param yMax : maximum position of boids on the axis Y.
      */
-    public Boids(float x, float y, float detectionRadius) {
+    public Boids(float x, float y, float detectionRadius, float xMax, float yMax) {
         this.position = new Vector(x, y);
         this.velocity = new Vector(0, 0);
         this.acceleration = new Vector(0, 0);
@@ -22,6 +28,8 @@ public class Boids {
             throw new IllegalArgumentException("Radius has to be positive");
         }
         this.detectionRadius = detectionRadius;
+        this.xMax = xMax;
+        this.yMax = yMax;
     }
 
     public Vector getPosition() {
@@ -57,6 +65,133 @@ public class Boids {
 
     public void setDetectionRadius(float detectionRadius) {
         this.detectionRadius = detectionRadius;
+    }
+
+    /**
+     * Rule of cohesion: move towards the perceived center
+     *
+     * @param boidsArrayList : list of boids of the space
+     * @return Vector corresponding to the cohesion force applied to this boid
+     */
+    public Vector rule1(ArrayList<Boids> boidsArrayList) {
+        Vector f = new Vector(0, 0);
+        for (Boids b : boidsArrayList) {
+            if (b != this) {
+                f.add(b.getPosition());
+            }
+        }
+        // f is the center of mass, not including itself
+        float n = boidsArrayList.size();
+        f.mult(1 / (n - 1));
+        // Move 1% towards the center
+        f.sub(this.position);
+        f.mult((float) 1 / 100);
+        return f;
+    }
+
+    /**
+     * Rule of separation: move away from other near boids
+     *
+     * @param boidsArrayList : list of boids of the space
+     * @return Vector corresponding to the separation force applied to this boid
+     */
+    public Vector rule2(ArrayList<Boids> boidsArrayList) {
+        Vector f = new Vector(0, 0);
+        for (Boids b : boidsArrayList) {
+            if (b != this) {
+                float distance = Vector.distance(this.position, b.getPosition());
+                if (distance < this.detectionRadius) {
+                    f.sub(Vector.sub(b.getPosition(), this.position));
+                }
+            }
+        }
+        f.mult(1 / (float) 20);
+        return f;
+    }
+
+    /**
+     * Rule of alignment: matching the "perceived velocity" of the group
+     *
+     * @param boidsArrayList : list of boids of the space
+     * @return Vector corresponding to the alignment force applied to this boid
+     */
+    public Vector rule3(ArrayList<Boids> boidsArrayList) {
+        Vector f = new Vector(0, 0);
+        for (Boids b : boidsArrayList) {
+            if (b != this) {
+                f.add(b.getVelocity());
+            }
+        }
+        // f is the perceived velocity
+        float n = boidsArrayList.size();
+        f.mult(1 / (n - 1));
+        // Add 1/8th to the boid's current velocity
+        f.sub(this.velocity);
+        f.mult(1 / (float) 8);
+        return f;
+    }
+
+    /**
+     * Rule of wind: apply wind force to the group
+     *
+     * @param boidsArrayList : list of boids of the space
+     * @return Vector corresponding to the wind force
+     */
+    public Vector rule4(ArrayList<Boids> boidsArrayList) {
+        return new Vector(0.1f, 0.9f);
+    }
+
+    /**
+     * Rule of borders: keeping the flock within the area use by the gui
+     *
+     * @param boidsArrayList : list of boids of the space
+     * @return Vector corresponding to the replace force applied to bj
+     */
+    public Vector rule5(ArrayList<Boids> boidsArrayList) {
+        Vector f = new Vector();
+        Vector boidPosition = this.position;
+
+        float mx = Math.max(10, Math.abs(this.velocity.getX()));
+        if (boidPosition.getX() < 0) {
+            f.setX(mx);
+        } else if (boidPosition.getX() > this.xMax) {
+            f.setX(-mx);
+        }
+
+        float my = Math.max(10, Math.abs(this.velocity.getY()));
+        if (boidPosition.getY() < 0) {
+            f.setY(my);
+        } else if (boidPosition.getY() > this.yMax) {
+            f.setY(-my);
+        }
+        return f;
+    }
+
+    /**
+     * Apply this boid's rules to modify its acceleration
+     *
+     * @param boidsArrayList : list of boids of the space
+     */
+    public void applyRules(ArrayList<Boids> boidsArrayList) {
+        Vector f1 = this.rule1(boidsArrayList);
+        Vector f2 = this.rule2(boidsArrayList);
+        Vector f3 = this.rule3(boidsArrayList);
+        Vector f4 = this.rule4(boidsArrayList);
+        Vector f5 = this.rule5(boidsArrayList);
+        // Calculate new acceleration
+        this.setAcceleration(f1);
+        this.getAcceleration().add(f2);
+        this.getAcceleration().add(f3);
+        this.getAcceleration().add(f4);
+        this.getAcceleration().add(f5);
+    }
+
+    public void updateVelocity() {
+        this.velocity.add(this.acceleration);
+    }
+
+    public void updatePosition() {
+        this.position.add(this.velocity);
     }
 
     @Override
