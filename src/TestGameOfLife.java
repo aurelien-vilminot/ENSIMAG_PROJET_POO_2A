9c8cc76d513b9134f9da4e908b40d243f6188b6b
Aurélien VILMINOT <49270516+aurelien-vilminot@users.noreply.github.com>
2021-11-10 19:39:23 +0100

    Improve Conway game

--- src/TestGameOfLife.java
+++ src/TestGameOfLife.java
@@ -10,13 +10,22 @@ public class TestGameOfLife {
 	public static void main(String[] args) {
 		GUISimulator gui = new GUISimulator(500, 500, Color.BLACK);
 		CellConway[] cellsConways = {
-				new CellConway(3, 3, 1),
-				new CellConway(5, 3, 1),
-				new CellConway(4, 4, 1),
-				new CellConway(5, 4, 1),
-				new CellConway(4, 5, 1),
+				new CellConway(10, 5, 1),
+				new CellConway(11, 5, 1),
+				new CellConway(11, 3, 1),
+				new CellConway(13, 4, 1),
+				new CellConway(14, 5, 1),
+				new CellConway(15, 5, 1),
+				new CellConway(16, 5, 1),
+				new CellConway(10, 25, 1),
+				new CellConway(11, 25, 1),
+				new CellConway(11, 23, 1),
+				new CellConway(13, 24, 1),
+				new CellConway(14, 25, 1),
+				new CellConway(15, 25, 1),
+				new CellConway(16, 25, 1),
 
 		};
-		gui.setSimulable(new GridSimulator(gui, "Conway", 50, cellsConways));
+		gui.setSimulable(new GridSimulator(gui, "Conway", 17, cellsConways));
 	}
 }
