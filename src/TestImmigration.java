1456383c678c775218e020a46fc3bde0af4ae843
Damien <thecsmine@gmail.com>
2021-11-13 14:24:37 +0100

    Test Immigration et Schelling random

--- src/TestImmigration.java
+++ src/TestImmigration.java
@@ -1,39 +1,36 @@
 import gui.GUISimulator;
+
 import java.awt.Color;
+import java.util.ArrayList;
+import java.util.Random;
 
 /**
  * Class to test the game of 'Immigration' composed of GridSimulator who implements
  * the interface Simulable using GUI, Grid, CellImmigration and Cell.
  */
 public class TestImmigration {
+    private static CellImmigration[] generateImmigrationCells(int nbStates, int nbCells, int guiSize, int cellSize) {
+        Random random = new Random();
+        int maxRange = (guiSize / cellSize) - 1;
+        ArrayList<CellImmigration> listOfCells = new ArrayList<>();
+        for (int i = 0; i < nbCells; i++) {
+            int x = random.nextInt(maxRange);
+            int y = random.nextInt(maxRange);
+            int state = random.nextInt(nbStates - 1) + 1;
+            listOfCells.add(new CellImmigration(x, y, state));
+        }
+        CellImmigration[] arr = new CellImmigration[listOfCells.size()];
+        return listOfCells.toArray(arr);
+    }
 
     public static void main(String[] args) {
-        GUISimulator gui = new GUISimulator(500, 500, Color.BLACK);
-        CellImmigration.setN(4);
-        CellImmigration[] cellImmigrations = {
-                new CellImmigration(1, 1, 3),
-                new CellImmigration(3, 1, 1),
-                new CellImmigration(4, 1, 1),
-                new CellImmigration(1, 2, 3),
-                new CellImmigration(2, 2, 1),
-                new CellImmigration(3, 2, 1),
-                new CellImmigration(4, 2, 1),
-                new CellImmigration(5, 2, 2),
-                new CellImmigration(1, 3, 1),
-                new CellImmigration(2, 3, 1),
-                new CellImmigration(3, 3, 3),
-                new CellImmigration(4, 3, 2),
-                new CellImmigration(5, 3, 2),
-                new CellImmigration(2, 4, 1),
-                new CellImmigration(3, 4, 2),
-                new CellImmigration(4, 4, 2),
-                new CellImmigration(5, 4, 2),
-                new CellImmigration(2, 5, 3),
-                new CellImmigration(3, 5, 2),
-                new CellImmigration(4, 5, 2),
-                new CellImmigration(5, 5, 1),
-
-        };
-        gui.setSimulable(new GridSimulator(gui, "Immigration", 50, cellImmigrations));
+        int guiSize = 500;
+        int cellSize = 20;
+        int nbStates = 4;
+        int nbCells = 100;
+        GUISimulator gui = new GUISimulator(guiSize, guiSize, Color.BLACK);
+        CellImmigration.setN(nbStates);
+        CellImmigration[] cellImmigrations = generateImmigrationCells(nbStates, nbCells, guiSize, cellSize);
+        gui.setSimulable(new GridSimulator(gui, "Immigration", cellSize, cellImmigrations));
     }
 }
--- src/TestSchelling.java
+++ src/TestSchelling.java
@@ -1,30 +1,38 @@
 import gui.GUISimulator;
 
 import java.awt.*;
+import java.util.ArrayList;
+import java.util.Random;
 
 /**
  * Class to test the game of Schelling composed of GridSimulator who implements
  * the interface Simulable using GUI, Grid, CellISchelling and Cell.
  */
 public class TestSchelling {
+    private static CellSchelling[] generateSchellingCells(int nbStates, int nbCells, int guiSize, int cellSize) {
+        Random random = new Random();
+        int maxRange = (guiSize / cellSize) - 1;
+        ArrayList<CellSchelling> listOfCells = new ArrayList<>();
+        for (int i = 0; i < nbCells; i++) {
+            int x = random.nextInt(maxRange);
+            int y = random.nextInt(maxRange);
+            int state = random.nextInt(nbStates - 1) + 1;
+            listOfCells.add(new CellSchelling(x, y, state));
+        }
+        CellSchelling[] arr = new CellSchelling[listOfCells.size()];
+        return listOfCells.toArray(arr);
+    }
+
     public static void main(String[] args) {
+        int guiSize = 500;
+        int cellSize = 20;
+        int nbStates = 4;
+        int nbCells = 100;
+        int k = 1;
         GUISimulator gui = new GUISimulator(500, 500, Color.BLACK);
-        CellSchelling.setK(1);
-        CellSchelling.setColors(4);
-        CellSchelling[] cellSchellings = {
-                new CellSchelling(1, 1, 3),
-                new CellSchelling(1, 5, 1),
-                new CellSchelling(4, 3, 1),
-                new CellSchelling(3, 2, 1),
-                new CellSchelling(3, 3, 1),
-                new CellSchelling(4, 2, 2),
-                new CellSchelling(9, 5, 2),
-                new CellSchelling(1, 9, 2),
-                new CellSchelling(7, 7, 2),
-                new CellSchelling(8, 7, 1),
-                new CellSchelling(8, 8, 1),
-                new CellSchelling(7, 8, 1),
-        };
-        gui.setSimulable(new GridSimulator(gui, "Schelling", 50, cellSchellings));
+        CellSchelling.setK(k);
+        CellSchelling.setColors(nbStates);
+        CellSchelling[] cellSchellings = generateSchellingCells(nbStates, nbCells, guiSize, cellSize);
+        gui.setSimulable(new GridSimulator(gui, "Schelling", cellSize, cellSchellings));
     }
 }
